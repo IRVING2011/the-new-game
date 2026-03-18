7936e77a31da3bcc4c2dde0dbd20cb71b246609d
IRVING2011 <arthur100625@gmail.com>
2026-03-19 03:45:03 +0800

    fix middleware runtime

--- middleware.js
+++ middleware.js
@@ -1,11 +1,9 @@
-import { NextResponse } from "next/server"
-
-export function middleware(request) {
-
-  return new NextResponse("Middleware Working", { status: 403 })
-
-}
-
 export const config = {
   matcher: "/:path*"
+}
+
+export default function middleware(request) {
+  return new Response("Middleware Working", {
+    status: 403
+  })
 }
