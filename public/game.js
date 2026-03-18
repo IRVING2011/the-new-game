ef3f975bc0fb745286748fd4dc2180522f7afdc8
IRVING2011 <arthur100625@gmail.com>
2026-03-19 04:51:57 +0800

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,15 +1,27 @@
 let playerScore=0
 let aiScore=0
 
+const icons={
+rock:"✊",
+paper:"✋",
+scissors:"✌"
+}
+
 function play(player){
 
 const choices=["rock","paper","scissors"]
+
 const ai=choices[Math.floor(Math.random()*3)]
+
+document.getElementById("playerHand").innerText=icons[player]
+document.getElementById("aiHand").innerText=icons[ai]
 
 let result=""
 
 if(player===ai){
+
 result="DRAW"
+
 }
 
 else if(
@@ -17,17 +29,20 @@ player==="rock"&&ai==="scissors"||
 player==="paper"&&ai==="rock"||
 player==="scissors"&&ai==="paper"
 ){
+
 result="YOU WIN"
 playerScore++
+
 }else{
+
 result="LOSE"
 aiScore++
+
 }
 
-document.getElementById("result").innerText=
-"AI chose: "+ai+" | "+result
+document.getElementById("result").innerText=result
 
-document.getElementById("player").innerText=playerScore
-document.getElementById("ai").innerText=aiScore
+document.getElementById("playerScore").innerText=playerScore
+document.getElementById("aiScore").innerText=aiScore
 
 }
