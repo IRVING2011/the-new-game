a47d5ef9f571a4be789064282c0f7e99a08de999
IRVING2011 <arthur100625@gmail.com>
2026-03-19 03:37:32 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server"
 
 export function middleware(request) {
+
   return new NextResponse("Middleware Working", { status: 403 })
+
+}
+
+export const config = {
+  matcher: "/:path*"
 }
