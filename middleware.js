920594db47c9dc36ee31f15085243e26898878b0
IRVING2011 <arthur100625@gmail.com>
2026-03-18 23:01:05 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,37 +1,21 @@
-const WHITELIST = (process.env.IP_WHITELIST || "").split(",")
+import { NextResponse } from "next/server";
 
-export const config = {
-  matcher: "/:path*"
-}
-
-export default function middleware(req) {
-
-  const url = new URL(req.url)
-
-  // 這些頁面不檢查
-  if (
-    url.pathname === "/403.html" ||
-    url.pathname === "/login.html" ||
-    url.pathname.startsWith("/api")
-  ) {
-    return
-  }
+export function middleware(req) {
 
   const ip =
     req.headers.get("x-forwarded-for") ||
     req.headers.get("x-real-ip") ||
-    ""
+    "";
 
-  // IP不在白名單
-  if (!WHITELIST.includes(ip)) {
-    return Response.redirect(new URL("/403.html", req.url))
+  const whitelist = (process.env.IP_WHITELIST || "").split(",");
+
+  if (!whitelist.includes(ip)) {
+    return NextResponse.redirect(new URL("/403.html", req.url));
   }
 
-  const cookie = req.headers.get("cookie") || ""
-
-  // 沒登入
-  if (!cookie.includes("session=valid")) {
-    return Response.redirect(new URL("/login.html", req.url))
-  }
-
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/game.html"]
+};
